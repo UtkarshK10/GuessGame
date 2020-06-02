84d4c9560a40f646a3e708a6f61e931f0066d108
Krishn <krishn.kumar2018@vitstudent.ac.in>
2020-06-02 20:08:44 +0530

    done

---
+++ makefile.sh
@@ -0,0 +1,11 @@
+
+all: README.md
+
+README.md: guessinggame.sh
+	touch README.md
+	echo "# guessinggame" > README.md
+	echo $$(date) >> README.md
+	echo "  \n" >> README.md
+	wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
+clean:
+	rm README.md
